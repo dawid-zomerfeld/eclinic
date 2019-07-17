97bad024155122b91ce83855e42c25913f3d323c
Dawid <kontakt.dawid.zomerfeld@gmail.com>
2019-07-17 15:03:57 +0200

    dodanie kolumn do encji Patient

--- src/main/java/pl/eclinic/EclinicApplication.java
+++ src/main/java/pl/eclinic/EclinicApplication.java
@@ -1,15 +1,7 @@
 package pl.eclinic;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import pl.eclinic.domain.Patient;
-import pl.eclinic.repository.PatientJpaRepository;
-import pl.eclinic.repository.PatientRepository;
-
-import java.util.Optional;
-import java.util.Set;
 
 @SpringBootApplication
 public class EclinicApplication {
--- src/main/java/pl/eclinic/domain/Patient.java
+++ src/main/java/pl/eclinic/domain/Patient.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @ToString
 @Getter
@@ -18,30 +20,38 @@ public class Patient {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="id_patient")
     private Long idPatient;
+
     @Column(name="first_name")
     private String firstName = "";
+
     @Column(name="last_name")
     private String lastName = "";
-    @Column(name="pesel", unique = true)
+
+    @Column(name="pesel")
     private String pesel = "";
+
     @Column(name="adress")
     private String adress = "";
+
+    @Size(min = 6, max = 6)
+    @Pattern(regexp = "[0-9]{2}\\-[0-9]{3}")
     @Column(name="postcode")
     private String postcode = "00-000";
-    @Column(name="twon")
+
+    @Column(name="town")
     private String town = "";
+
     @Column(name="telephone")
     private String telephone = "";
-    @Column(name="email", unique = true)
-    private String email = "";
-    @Column(name="password")
+
+    @Column(name="email")
+     private String email = "";
+
+   @Column(name="password")
     private String password = "";
 
 
 
 
-    public Patient(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+
 }
